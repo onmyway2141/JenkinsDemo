c208fdb283178663224e538396cf841a1639efbc
shama <shamashaikh0130@gmail.com>
2024-09-05 13:17:49 +0530

    Driver setup added

--- src/test/java/pack1Demo/TestSetup.java
+++ src/test/java/pack1Demo/TestSetup.java
@@ -1,5 +1,24 @@
 package pack1Demo;
 
-public class TestSetup {
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestSetup {
+		
+	public static WebDriver driver;
+	
+	public void setup() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.get("https://www.youtube.com/");
+		driver.manage().window().maximize();
+	//	driver.manage().wait();
+		System.out.println("Driver lounch successfully");
+		driver.close();
+		
+	}
 }
---
+++ src/test/java/pack2Demo/BrowserTest.java
@@ -0,0 +1,15 @@
+package pack2Demo;
+
+import org.testng.annotations.Test;
+import pack1Demo.*;
+
+public class BrowserTest {
+@Test
+
+public void drivertest() throws InterruptedException
+{
+	
+	TestSetup setupobj=new TestSetup();
+	setupobj.setup();
+}
+}
